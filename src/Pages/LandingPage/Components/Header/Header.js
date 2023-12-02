600cd3aa83102e2dab2ef2c5e40207cd72c35e46
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-02 14:28:56 +0200

    implemented header

--- src/Pages/LandingPage/Components/Header/Header.js
+++ src/Pages/LandingPage/Components/Header/Header.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Header.css";
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="landing-header">
+      <img src={require("../images/logo-white.png")} alt="logo" />
+      <nav>
+        <ul>
+          <li>
+            <a href="jsx-a11y/anchor-is-valid">About Us</a>
+          </li>
+          <li>
+            <a href="jsx-a11y/anchor-is-valid">doctors</a>
+          </li>
+          <li>
+            <a href="jsx-a11y/anchor-is-valid">Patients</a>
+          </li>
+          <button>Log-In</button>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
