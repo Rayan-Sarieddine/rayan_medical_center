70d0b25ef06db69b5386fa0b682cfc2e1a30993c
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-02 19:45:17 +0200

    implemented patients page

--- src/Pages/PatientPage/Components/BookAppointment/BookAppointments.js
+++ src/Pages/PatientPage/Components/BookAppointment/BookAppointments.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./BookAppointment.css";
-function BookAppointments() {
-  return <div>BookAppointments</div>;
+function BookAppointments({ patient }) {
+  const [showBookAppointmentsBody, setShowBookAppointmentsBody] =
+    useState(false);
+  const toggleBookAppointmentsBody = () => {
+    setShowBookAppointmentsBody(!showBookAppointmentsBody);
+  };
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateSelection = (event) => {
+    const d = event.target.value;
+    const newd = new Date(d);
+    setSelectedDate(newd.toLocaleString());
+  };
+  return (
+    <div className="book-appointments">
+      <div className="book-appointments_header">
+        <h3>
+          {patient.patient_name.split(" ")[0].charAt(0).toUpperCase() +
+            patient.patient_name.split(" ")[0].slice(1).toLowerCase()}
+          , Book your future appointments here!
+        </h3>
+        <div onClick={toggleBookAppointmentsBody}>&#9660;</div>
+      </div>
+      {showBookAppointmentsBody && (
+        <div className="book-appointments_body">
+          <h2>Book Appointments</h2>
+          <label for="book-appointmentDateTime">
+            Select Appointment's Date and Time:
+          </label>
+          <input
+            type="datetime-local"
+            id="book-appointmentDateTime"
+            name="book-appointmentDateTime"
+            onChange={handleDateSelection}
+          />
+
+          <div className="selected-date">
+            Selected Date: {selectedDate ? selectedDate : "No date selected"}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default BookAppointments;
