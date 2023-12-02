c6917c71fb98f523e226f9aaaa207bfd9e4f355b
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-02 14:55:55 +0200

    implemented hero

--- src/Pages/LandingPage/Components/Hero/Hero.js
+++ src/Pages/LandingPage/Components/Hero/Hero.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Hero.css";
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <h1>WE CARE</h1>
+      <p>for you!</p>
+    </div>
+  );
 }
 
 export default Hero;
