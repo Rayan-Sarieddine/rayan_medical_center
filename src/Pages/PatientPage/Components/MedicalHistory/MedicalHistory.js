6c0064f947a575e3c8bc10cee01b117499afbd4c
Rayan-Sarieddine <rayan2.sarieddine@gmail.com>
2023-12-05 02:51:59 +0200

    fixed section not showing when data is not available

--- src/Pages/PatientPage/Components/MedicalHistory/MedicalHistory.js
+++ src/Pages/PatientPage/Components/MedicalHistory/MedicalHistory.js
@@ -29,16 +29,16 @@ function MedicalHistory({ patient }) {
   }, []);
   return (
     <div className="medical-history">
+      <div className="medical-history_header">
+        <h3>
+          {patient.patient_name.split(" ")[0].charAt(0).toUpperCase() +
+            patient.patient_name.split(" ")[0].slice(1).toLowerCase()}
+          's Medical History
+        </h3>
+        <div onClick={toggleMedicalHistoryBody}>&#9660;</div>
+      </div>
       {medicalHistory && (
         <>
-          <div className="medical-history_header">
-            <h3>
-              {patient.patient_name.split(" ")[0].charAt(0).toUpperCase() +
-                patient.patient_name.split(" ")[0].slice(1).toLowerCase()}
-              's Medical History
-            </h3>
-            <div onClick={toggleMedicalHistoryBody}>&#9660;</div>
-          </div>
           {showMedicalHistoryBody && (
             <div className="medical-history_body">
               {medicalHistory.map((con, ind) => {
